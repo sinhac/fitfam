70096b10193cf1abd7a4e2ae70b56eb649c5baa6
fishes3 <sfisher1911@gmail.com>
2017-03-20 16:58:46 -0400

    Trying to pull

--- fitfam/Resources/Resource.Designer.cs
+++ fitfam/Resources/Resource.Designer.cs
@@ -60,14 +60,11 @@ namespace fitfam
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050003
-			public const int button1 = 2131034115;
-			
-			// aapt resource value: 0x7f050004
-			public const int button2 = 2131034116;
-			
 			// aapt resource value: 0x7f050002
-			public const int checkedTextView1 = 2131034114;
+			public const int button1 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int button2 = 2131034115;
 			
 			// aapt resource value: 0x7f050001
 			public const int imageView1 = 2131034113;
