c964354375b4d43e55198871d2af8dbc001c5a4a
fishes3 <sfisher1911@gmail.com>
2017-03-22 18:36:41 -0400

    Committing again for merge

--- fitfam/Resources/Resource.Designer.cs
+++ fitfam/Resources/Resource.Designer.cs
@@ -44,11 +44,11 @@ namespace fitfam
 		public partial class Color
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int theme_custom1_background = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int theme_custom1_background = 2131099648;
 			
-			// aapt resource value: 0x7f050001
-			public const int theme_devicedefault_background = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int theme_devicedefault_background = 2131099649;
 			
 			static Color()
 			{
@@ -64,10 +64,34 @@ namespace fitfam
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int clock = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int logo = 2130837505;
+			public const int face = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int fitfamLogo1smaller = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int gold_button = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int gold_logo = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int gold_plate = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int home = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int Icon = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int logo = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int message = 2130837513;
 			
 			static Drawable()
 			{
@@ -85,17 +109,17 @@ namespace fitfam
 			// aapt resource value: 0x7f070001
 			public const int actionMenuView1 = 2131165185;
 			
-			// aapt resource value: 0x7f070006
-			public const int button1 = 2131165190;
-			
-			// aapt resource value: 0x7f070007
-			public const int button2 = 2131165191;
+			// aapt resource value: 0x7f070008
+			public const int button1 = 2131165192;
 			
 			// aapt resource value: 0x7f070009
-			public const int button3 = 2131165193;
+			public const int button2 = 2131165193;
 			
-			// aapt resource value: 0x7f070008
-			public const int button4 = 2131165192;
+			// aapt resource value: 0x7f07000c
+			public const int button3 = 2131165196;
+			
+			// aapt resource value: 0x7f07000b
+			public const int button4 = 2131165195;
 			
 			// aapt resource value: 0x7f070002
 			public const int imageButton1 = 2131165186;
@@ -109,11 +133,20 @@ namespace fitfam
 			// aapt resource value: 0x7f070005
 			public const int imageButton4 = 2131165189;
 			
-			// aapt resource value: 0x7f07000a
-			public const int imageView1 = 2131165194;
+			// aapt resource value: 0x7f07000d
+			public const int imageView1 = 2131165197;
 			
 			// aapt resource value: 0x7f070000
 			public const int imageView5 = 2131165184;
+			
+			// aapt resource value: 0x7f070006
+			public const int linearLayout1 = 2131165190;
+			
+			// aapt resource value: 0x7f070007
+			public const int space1 = 2131165191;
+			
+			// aapt resource value: 0x7f07000a
+			public const int space2 = 2131165194;
 			
 			static Id()
 			{
@@ -169,11 +202,14 @@ namespace fitfam
 		public partial class Style
 		{
 			
-			// aapt resource value: 0x7f060000
-			public const int Theme_Custom = 2131099648;
+			// aapt resource value: 0x7f050001
+			public const int Theme_Custom = 2131034113;
 			
-			// aapt resource value: 0x7f060001
-			public const int Theme_Custom1 = 2131099649;
+			// aapt resource value: 0x7f050002
+			public const int Theme_Custom1 = 2131034114;
+			
+			// aapt resource value: 0x7f050000
+			public const int button_text = 2131034112;
 			
 			static Style()
 			{
