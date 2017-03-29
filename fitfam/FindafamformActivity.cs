7a8f8f7a2a453cadf46e49ad0b62c76bf58c5691
Chandrika Sinha <sinhac@rpi.edu>
2017-03-28 21:47:43 -0400

    fixed package errors

--- fitfam/FindafamformActivity.cs
+++ fitfam/FindafamformActivity.cs
@@ -13,8 +13,29 @@ namespace fitfam
             SetContentView(Resource.Layout.Findafamform);
 
             // Create your application here
-            Button button2 = FindViewById<Button>(Resource.Id.button2);
 
+            var activity = FindViewById<EditText>(Resource.Id.activity);
+            var activityTag = "";
+            activity.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
+            {
+                activityTag = e.Text.ToString();
+            };
+
+            var cityZip = FindViewById<EditText>(Resource.Id.cityzip);
+            var cityZipInput = "";
+            activity.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
+            {
+                cityZipInput = e.Text.ToString();
+            };
+
+            var experience = FindViewById<EditText>(Resource.Id.experience);
+            var experienceInput = "";
+            activity.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
+            {
+                experienceInput = e.Text.ToString();
+            };
+
+            Button button2 = FindViewById<Button>(Resource.Id.button2);
             button2.Click += delegate {
                 StartActivity(typeof(matchesActivity));
             };
