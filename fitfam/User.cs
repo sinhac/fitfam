db1825c530dbbfcd64c2f4fc1b155c417304c73b
joshjcbsn <joshjcbsn@gmail.com>
2017-03-29 22:46:29 -0400

    added get set to user

--- fitfam/User.cs
+++ fitfam/User.cs
@@ -15,20 +15,53 @@ namespace fitfam
     class User
     {
         private string userId;
+        public string UserId
+        {
+            get { return userId; }
+        }
         private string username;
+        public string Username
+        {
+            get { return username; }
+        }
         private List<string> activities;
         private Dictionary<string, bool> availability;
+        private bool availabilitySet;
+        public bool AvailabilitySet
+        {
+            get { return availabilitySet; }
+        }
         private string bio;
-        private string experienceLevel; //change to the type where it's 1 of a couple options
+        public string Bio
+        {
+            get { return bio; }
+            set { bio = value; }
+        }
+        private string experienceLevel; //change to enum type
+        public string ExperienceLevel
+        {
+            get { return experienceLevel; }
+            set { experienceLevel = value; }
+        }
         private List<Group> fitFams;
         private List<User> friends;
         private string gender;
+        public string Gender
+        {
+            get { return gender; }
+        }
         private string pic;
+        public string Pic
+        {
+            get { return pic; }
+            set { pic = value; }
+        }
         private List<Event> sharedEvents;
 
-        public User()
+        public User(string userId)
         {
-
+            //get user data
         }
+
     }
 }
